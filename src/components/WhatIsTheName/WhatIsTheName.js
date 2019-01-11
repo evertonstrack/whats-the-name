48bc5e9e79b8237907abd1245c4bb8ff6437a7e6
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-01-11 14:26:52 -0200

    feat: Searching names

--- src/components/WhatIsTheName/WhatIsTheName.js
+++ src/components/WhatIsTheName/WhatIsTheName.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
 import './what-is-the-name.scss';
 
+const suggestedNames = [
+    'Wallace',
+    'Warley',
+    'Warlen',
+    'Welton',
+    'Waldemar',
+    'Waldir',
+    'Waldo',
+    'Waldomiro',
+    'Walgi',
+    'Walter',
+    'Wilmar',
+    'Wilson',
+    'Waldisnei',
+    'Walisson',
+    'Walton'
+];
+
 export class WhatIsTheName extends Component {
 
+  state = {
+    name: null
+  };
+
+  componentDidMount() {
+    this.getRandomName()
+        .then(name => {
+          this.setState({ name });
+        })
+  }
+
+  getRandomName() {
+    // Get a suggested name from array
+    const suggested = suggestedNames[Math.floor(Math.random() * suggestedNames.length)];
+    // Search for it and append the name variations from IBGE
+    return fetch(`https://servicodados.ibge.gov.br/api/v1/censos/nomes/basica?nome=${suggested}`)
+      .then(res => res.json())
+      .then(data => {
+          const otherNames = data[0].nomes.split(',');
+          suggestedNames.push(...otherNames);
+          return suggestedNames[Math.floor(Math.random() * suggestedNames.length)];
+      });
+  }
+
   render() {
-    
+    const { name } = this.state;
     return(
       <section>
-        <h2>Walter</h2>
+        { name
+            ? <h2>{name}</h2>
+            : <h2>???</h2>
+        }
       </section>
     );
   }
