c5fa586da57b6affbb37be47e08baec7df38aee1
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-01-11 14:51:32 -0200

    feat: Adds slack webhook

--- src/components/WhatIsTheName/WhatIsTheName.js
+++ src/components/WhatIsTheName/WhatIsTheName.js
@@ -1,23 +1,6 @@
 import React, { Component } from 'react';
 import './what-is-the-name.scss';
-
-const suggestedNames = [
-    'Wallace',
-    'Warley',
-    'Warlen',
-    'Welton',
-    'Waldemar',
-    'Waldir',
-    'Waldo',
-    'Waldomiro',
-    'Walgi',
-    'Walter',
-    'Wilmar',
-    'Wilson',
-    'Waldisnei',
-    'Walisson',
-    'Walton'
-];
+import { SUGGESTED_NAMES, SLACK_WEBHOOK } from '../../config';
 
 export class WhatIsTheName extends Component {
 
@@ -29,19 +12,31 @@ export class WhatIsTheName extends Component {
     this.getRandomName()
         .then(name => {
           this.setState({ name });
+          // Send a message to slack
+          fetch(SLACK_WEBHOOK, {
+              method: 'POST',
+              mode: 'no-cors',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ text: `Meu nome é ${name}.` })
+          });
         })
   }
 
   getRandomName() {
     // Get a suggested name from array
-    const suggested = suggestedNames[Math.floor(Math.random() * suggestedNames.length)];
+    const suggested = SUGGESTED_NAMES[Math.floor(Math.random() * SUGGESTED_NAMES.length)];
     // Search for it and append the name variations from IBGE
     return fetch(`https://servicodados.ibge.gov.br/api/v1/censos/nomes/basica?nome=${suggested}`)
       .then(res => res.json())
       .then(data => {
-          const otherNames = data[0].nomes.split(',');
-          suggestedNames.push(...otherNames);
-          return suggestedNames[Math.floor(Math.random() * suggestedNames.length)];
+          // Variations in PascalCase
+          const otherNames = data[0].nomes.split(',')
+              .map(name => name.replace(/\w+/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase()));
+          SUGGESTED_NAMES.push(...otherNames);
+          return SUGGESTED_NAMES[Math.floor(Math.random() * SUGGESTED_NAMES.length)];
       });
   }
 
---
+++ src/config.js
@@ -0,0 +1,19 @@
+export const SUGGESTED_NAMES = [
+    'Wallace',
+    'Warley',
+    'Warlen',
+    'Welton',
+    'Waldemar',
+    'Waldir',
+    'Waldo',
+    'Waldomiro',
+    'Walgi',
+    'Walter',
+    'Wilmar',
+    'Wilson',
+    'Waldisnei',
+    'Walisson',
+    'Walton'
+];
+
+export const SLACK_WEBHOOK = 'https://hooks.slack.com/services/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';
